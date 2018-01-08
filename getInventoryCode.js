9d5984cb4f32e68ecf2d9ab89eb981e756eb704c
Vidit <vidit-agarwal@users.noreply.github.com>
2018-01-08 22:15:45 +0530

    Update getInventoryCode.js

--- getInventoryCode.js
+++ getInventoryCode.js
@@ -1,5 +1,5 @@
 const li = require("steam-inventory");
-li("76561198306639779", 730, 2, (items, error) => {
+li("xxxx", 730, 2, (items, error) => {
   if(error) {
     return console.log("Error, while getting user items. Please check settings or user inventory is hidden.");
   } 
